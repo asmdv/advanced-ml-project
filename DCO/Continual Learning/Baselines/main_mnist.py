c86d702543c0abb4f771717389ad1df54a5eec37
Asif Mammadov <mammadovwork@gmail.com>
2024-04-21 20:46:38 -0400

    improve experiment naming

--- DCO/Continual Learning/Baselines/main_mnist.py
+++ DCO/Continual Learning/Baselines/main_mnist.py
@@ -88,7 +88,7 @@ class ReplayBufferCL():
 def create_experiment_path(args):
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d_%H_%M_%S")
-    experiment_name = f"experiments/exp_{args.cl_dataset}_{args.cl_method}_n_tasks_{args.num_tasks}_epochs_{args.lr_epochs}_{args.cl_epochs}_rank_{args.rank}_max_layers_{args.max_allowed_added_layers}_{formatted_time}"
+    experiment_name = f"experiments/exp_{args.cl_dataset}_{args.cl_method}_n_tasks_{args.num_tasks}_epochs_{args.lr_epochs}_{args.cl_epochs}_threshold_{args.cl_error_threshold}_max_layers_{args.max_allowed_added_layers}_{formatted_time}"
     create_directory_if_not_exists(experiment_name)
     create_directory_if_not_exists(f"{experiment_name}/plots")
     return experiment_name
