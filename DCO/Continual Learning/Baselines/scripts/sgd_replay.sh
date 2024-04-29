6c05e1c650a743ab9f1f70f2c444bd0d41818ebe
Asif Mammadov <mammadovwork@gmail.com>
2024-04-29 16:59:13 -0400

    fix epoch size

--- DCO/Continual Learning/Baselines/scripts/sgd_replay.sh
+++ DCO/Continual Learning/Baselines/scripts/sgd_replay.sh
@@ -1,5 +1,5 @@
 # Replay
-python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 5 --cl_epochs 5 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 5
+python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 5 --cl_epochs 5 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 5 &
 
 python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 5 --cl_epochs 5 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 10 &
 
