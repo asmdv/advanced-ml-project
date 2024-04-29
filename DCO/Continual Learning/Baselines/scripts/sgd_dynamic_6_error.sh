21bf0e3352706388ef8313c74129e68c9fc8673d
Asif Mammadov <mammadovwork@gmail.com>
2024-04-28 21:28:07 -0400

    upgrade scripts

--- DCO/Continual Learning/Baselines/scripts/sgd_dynamic_6_error.sh
+++ DCO/Continual Learning/Baselines/scripts/sgd_dynamic_6_error.sh
@@ -8,4 +8,5 @@ python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --
 python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0  --cl_error_threshold 6 --max_allowed_added_layers 10 --freeze --added_layer_conf 4,0,0 &
 
 python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0  --cl_error_threshold 6 --max_allowed_added_layers 10 --freeze --added_layer_conf 5,0,0 &
+wait
 
--- DCO/Continual Learning/Baselines/scripts/sgd_replay.sh
+++ DCO/Continual Learning/Baselines/scripts/sgd_replay.sh
@@ -8,3 +8,4 @@ python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --
 python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 40 &
 
 python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 64 &
+wait
