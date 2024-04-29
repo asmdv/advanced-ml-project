157e7bfc262c8433a8736efd17b41893005f7ba4
Asif Mammadov <mammadovwork@gmail.com>
2024-04-28 23:18:26 -0400

    add flushing

--- DCO/Continual Learning/Baselines/path_utils.py
+++ DCO/Continual Learning/Baselines/path_utils.py
@@ -10,7 +10,7 @@ class Tee(object):
     def write(self, obj):
         for f in self.files:
             f.write(obj)
-            # f.flush()  # If you want the output to be visible immediately
+            f.flush()  # If you want the output to be visible immediately
 
     def flush(self):
         for f in self.files:
