b13a0d5b06b65a352bf6cefcc0d266b41cb0379a
Asif Mammadov <mammadovwork@gmail.com>
2024-04-29 16:56:15 -0400

    add scripts

--- DCO/Continual Learning/Baselines/scripts/sgd_replay.sh
+++ DCO/Continual Learning/Baselines/scripts/sgd_replay.sh
@@ -1,5 +1,5 @@
 # Replay
-python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 5 &
+python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 2 --cl_epochs 2 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 5
 
 python ../main_mnist.py --main_optimizer 'sgd'  --lr_epochs 10 --cl_epochs 10 --main_online_lr 1e-2 --train-batch-size 128 --wd 1e-3 --cl_method 'sgd'  --cl_dataset permuted_mnist --num_tasks 5 --rank 0 --replay_buffer_batch_size 10 &
 
